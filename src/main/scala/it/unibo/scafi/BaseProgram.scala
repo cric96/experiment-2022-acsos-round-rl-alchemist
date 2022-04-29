885c2779f16130542ff91ecbbeb1577d7dd5044e
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2022-04-29 17:05:46 +0200

    add q tables

--- src/main/scala/it/unibo/scafi/BaseProgram.scala
+++ src/main/scala/it/unibo/scafi/BaseProgram.scala
@@ -1,9 +1,7 @@
 package it.unibo.scafi
 import it.unibo.alchemist.model.scafi.ScafiIncarnationForAlchemist._
-class BaseProgram extends AggregateProgram with StandardSensors with ScafiAlchemistSupport {
-  override def main(): Any = rep(Double.PositiveInfinity) { g =>
-    mux(source)(0.0)(minHoodPlus(nbr(g) + nbrRange()))
-  }
+class BaseProgram extends AggregateProgram with StandardSensors with ScafiAlchemistSupport with BlockG {
+  override def main(): Any = classicGradient(source)
 
   def source = node.has("source")
 }
